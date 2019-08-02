887f8207b958b443d37dc7fb2a0c4f25c24d64b9
Lio <lionell.marquez@gmail.com>
2019-08-01 23:23:49 -0400

    Heroku deployment

--- googlebooks/src/App.js
+++ googlebooks/src/App.js
@@ -19,6 +19,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="/" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
